4407108c3f3d125dd5f0ddba5edd97c92118c036
Patrice White <pwhite2@xula.edu>
2016-10-25 17:08:21 -0500

    Sends an email to rest your password

---
+++ PasswordRecovery/LogInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogInViewController.swift
+//  
+//
+//  Created by Patrice white on 10/24/16.
+//
+//
+
+import UIKit
+import Firebase
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signUp(_ sender: AnyObject) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
