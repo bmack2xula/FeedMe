7a8ae9e2c71ed14430e28fa32592c4497228613a
Patrice white <Patrice@Patrices-MacBook-Pro.local>
2016-10-25 10:23:59 -0500

    recovers password

---
+++ LogInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LogInViewController.swift
+//  
+//
+//  Created by Patrice white on 10/24/16.
+//
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
