06742b456163e0b96984f1202da4f9e433872d06
Patrice White <pwhite2@xula.edu>
2016-10-27 01:11:11 -0500

    displays a random quote when app loads

--- AppQuote/ViewController.swift
+++ AppQuote/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    
+    let funFacts = Facts()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // show fact when app loads
+        funFactLabel.text = funFacts.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
